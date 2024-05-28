999030478bd720f1c3beae7881d74291377e8fda
kishkolh <kishkolh@fel.cvut.cz>
2024-05-28 22:14:38 +0200

    add svg

--- quiz.js
+++ quiz.js
@@ -1,18 +1,24 @@
 let question_count = 0;
 let result_colour = '';
 let total_weight = 0;
+
 let currentResult = new Map;
-let currentState = { currentQuestion: 0, weight: 0 };
+let currentState = {currentQuestion: 0, weight: 0};
 localStorage.removeItem("total_weight");
 localStorage.removeItem("result_colour");
 
 /* Show questions */
-window.onload = function(){
+window.onload = function () {
+    const userName = localStorage.getItem('userName');
+    // Display the name in the welcome message or wherever needed
+    if (userName) {
+        document.querySelector('.name').innerText = userName;
+    }
     show(question_count);
 };
 
 /* Save page state */
-window.history.pushState({ currentQuestion: 0, weight: 0 }, null, window.location.href);
+window.history.pushState({currentQuestion: 0, weight: 0}, null, window.location.href);
 
 /* Handle browser navigation button click */
 window.onpopstate = function (event) {
@@ -20,7 +26,7 @@ window.onpopstate = function (event) {
     if (!event.state || event.state.currentQuestion < currentState.currentQuestion) {
         if (!event.state) {
             history.back();
-        } else  {
+        } else {
             currentState = event.state;
             total_weight = total_weight - currentResult.get(question_count - 1);
             question_count--;
@@ -30,13 +36,13 @@ window.onpopstate = function (event) {
 }
 
 /* Handle question option click */
-document.getElementById("questions").addEventListener("click", function(event) {
+document.getElementById("questions").addEventListener("click", function (event) {
     if (event.target && event.target.matches("li.option")) {
         let questionIndex = question_count;
         let optionWeight = parseInt(event.target.getAttribute("data-weight"));
         total_weight += optionWeight;
         localStorage.setItem("total_weight", total_weight);
-        currentState = { currentQuestion: question_count + 1, weight: optionWeight };
+        currentState = {currentQuestion: question_count + 1, weight: optionWeight};
         history.pushState(currentState, null, document.location.href);
         currentResult.set(question_count, optionWeight);
 
@@ -50,36 +56,46 @@ document.getElementById("questions").addEventListener("click", function(event) {
             setTimeout(() => window.location.href = "final.html", 3000);
         }
     }
-    });
+});
 
 /* Show questions */
-    function show(count) {
-        let question = document.getElementById("questions");
-        let optionsHTML = "";
-        questions[count].options.forEach(option => {
-            optionsHTML += `<li class="option" data-weight="${option.weight}">${option.text}</li>`;
-        });
+function show(count) {
+    let question = document.getElementById("questions");
+    let optionsHTML = "";
+    questions[count].options.forEach(option => {
+        optionsHTML += `<li class="option" data-weight="${option.weight}">${option.text}</li>`;
+    });
 
-        question.innerHTML = `<h2>Q${count + 1}. ${questions[count].question}</h2>
+    question.innerHTML = `<h2>Q${count + 1}. ${questions[count].question}</h2>
                       <ul class="option_group">${optionsHTML}</ul>`;
-    }
+}
 
 /* Calculate character type results */
-    function calculate_result(weight){
-        if (weight <= 40 && weight >= 35){
-            result_colour = "Red";
-        }
-        else if (weight < 35 && weight >= 25){
-            result_colour = "Yellow";
-        }
-        else if (weight < 25 && weight >= 15){
-            result_colour = "Green";
-        }
-        else {
-            result_colour = "Blue";
-        }
-        localStorage.setItem("result_colour", result_colour); // Store result colour in local storage
+function calculate_result(weight) {
+    if (weight <= 40 && weight >= 35) {
+        result_colour = "Red";
+    } else if (weight < 35 && weight >= 25) {
+        result_colour = "Yellow";
+    } else if (weight < 25 && weight >= 15) {
+        result_colour = "Green";
+    } else {
+        result_colour = "Blue";
     }
+    localStorage.setItem("result_colour", result_colour); // Store result colour in local storage
+}
 
+// svg
+let svg = document.querySelector('.svg-icon');
+svg.addEventListener('click', function(e) {
+    const modal = document.querySelector('#modal');
 
+    svg.classList.add('open');
+    modal.classList.add('open');
+
+    const modalCloseBtn = document.querySelector('.modal-close svg');
+    modalCloseBtn.addEventListener('click', () => {
+        modal.classList.remove('open');
+        svg.classList.remove('open');
+    })
+});
 
